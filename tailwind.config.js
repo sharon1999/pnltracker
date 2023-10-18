1ebe3126e9bacf336fd93dea179d403462c331a3
sharon1999 <sharonantony51@gmail.com>
2023-10-18 11:39:29 +0530

    Added Poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"]
+      },
+    },
   },
   plugins: [],
 };
